d71004d0535ba67c6538a7bdb92e5cddf37fbfc2
TanHuynh <tanhuynh7894.work@gmail.com>
2025-06-16 22:25:08 +0700

    fix lỗi otp

--- BE/routes/otp.js
+++ BE/routes/otp.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { verifyOtp } = require('../controllers/otpController');
+const { verifyOtp } = require('../controllers/otpControllers');
 
 router.post('/verify-otp', verifyOtp);
 
