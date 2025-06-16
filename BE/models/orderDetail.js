1e39f9702eb34e46bec50556cd391d5b86e31663
TanHuynh <tanhuynh7894.work@gmail.com>
2025-06-16 23:41:00 +0700

    sửa file orderDetail trong models và chuẩn bị server.js cho function topSeller

--- BE/models/orderDetail.js
+++ BE/models/orderDetail.js
@@ -27,7 +27,7 @@ const OrderDetail = sequelize.define('OrderDetail', {
   timestamps: false
 });
 
-OrderDetail.associate = (models) => {
+  OrderDetail.associate = (models) => {
   OrderDetail.belongsTo(models.Order, { foreignKey: 'orderId', as: 'order' });
   OrderDetail.belongsTo(models.Product, { foreignKey: 'productId', as: 'product' });
 };
