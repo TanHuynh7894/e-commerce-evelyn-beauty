abf5f4ac49ff0b6cc2d60513a3bc5648ec5aafec
TanHuynh <tanhuynh7894.work@gmail.com>
2025-07-10 12:15:50 +0700

    chinh sua order

--- BE/routes/orderRoutes.js
+++ BE/routes/orderRoutes.js
@@ -4,16 +4,19 @@ const orderController = require('../controllers/order.controllers');
 const { verifyToken, requireRole , logDbAccess } = require('../middlewares/auth');
 
 
-// Chỉ Customer được phép tạo order
-router.post('/', verifyToken, requireRole('CU'), orderController.createOrder);
+// // Chỉ Customer được phép tạo order
+// router.post('/', verifyToken, requireRole('CU'), orderController.createOrder);
 
-// Customer chỉ xem được đơn hàng của mình
-router.get('/my-orders', verifyToken, requireRole('CU'), orderController.getCustomerOrders);
+// // Customer chỉ xem được đơn hàng của mình
+// router.get('/my-orders', verifyToken, requireRole('CU'), orderController.getCustomerOrders);
 
-// Staff/OS xem được tất cả đơn hàng + ghi log
-router.get('/', verifyToken, requireRole('SF', 'OS'), logDbAccess, orderController.getAllOrders);
+// // Staff/OS xem được tất cả đơn hàng + ghi log
+// router.get('/', verifyToken, requireRole('SF', 'OS'), logDbAccess, orderController.getAllOrders);
 
-// Chỉ Staff/OS được phép cập nhật status đơn hàng + ghi log
-router.put('/:orderId', verifyToken, requireRole('SF', 'OS'), logDbAccess, orderController.updateOrderStatus);
+// // Chỉ Staff/OS được phép cập nhật status đơn hàng + ghi log
+// router.put('/:orderId', verifyToken, requireRole('SF', 'OS'), logDbAccess, orderController.updateOrderStatus);
+
+//lấy thông tin giao hàng
+router.get('/', verifyToken, orderController.getDeliveryAddress);
 
 module.exports = router;
