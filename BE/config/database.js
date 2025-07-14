3bb8a5c2d5392595b4b4e3fb47ae4052dd1aa616
Khanh <kn01234444@gmail.com>
2025-07-15 03:19:27 +0700

    sua lai ket noi db

--- BE/config/database.js
+++ BE/config/database.js
@@ -1,8 +1,15 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('evelynBeauty', 'root', '12345678', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+  }
+);
 
 module.exports = sequelize;
